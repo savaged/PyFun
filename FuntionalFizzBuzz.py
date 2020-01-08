02066698c8e056bf003133192ed08bf2aaee01a5
savaged <savagedavid@gmail.com>
2020-01-08 19:16:29 +0000

    Python FizzBuzz (functional style)

---
+++ FuntionalFizzBuzz.py
@@ -0,0 +1,30 @@
+
+def getVerb(i):
+    value = ""
+    r = i % 3
+    if r == 0:
+        value = "Fizz"
+    r = i % 5
+    if r == 0:
+        value = value + "Buzz"
+    if value == "":
+        value = i
+    return value
+
+def getVerbs(integers = []):
+    verbs = []
+    for i in integers:
+        verbs.append(getVerb(i))
+    return verbs
+
+def main():
+    start = 1
+    iterations = 16
+    end = start + iterations
+    integers = range(start, end)
+    verbs = getVerbs(integers)
+    for verb in verbs:
+        print(verb)
+    print("Done!")
+
+main()
